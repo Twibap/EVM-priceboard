8fab8462a1f94e62d423a58374317ebff14b5e4a
TK Yu <tky476@gmail.com>
2018-12-11 16:48:03 +0900

    Dockerized

--- app_module/database.js
+++ app_module/database.js
@@ -3,7 +3,8 @@ const colors = require('./colors');
 // Database for price
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-var dbUrl = 'mongodb://localhost/';
+//var dbUrl = 'mongodb://localhost/';
+var dbUrl = 'mongodb://172.17.0.2/';
 var dbName = 'EVM';
 
 db.on('error', console.error);
