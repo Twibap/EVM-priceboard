6c364a7bf0503c57704ddf8c1a64a54e69975677
TK Yu <tky476@gmail.com>
2019-02-04 19:09:09 +0900

    DB 주소 입력 가능

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,9 @@ RUN npm install
 # 앱 복사
 COPY . .
 
+# 호스트와 연결할 포트 설정
 EXPOSE 8080
-CMD ["npm","start"]
+
+# 앱 실행
+# docker run -e DATABASE_URL=127.0.0.1 -e DATABASE_PORT=27017 --name evm-price -p 8080:8080 -d twibap/evm-price
+CMD node app.js -d $DATABASE_URL -p $DATABASE_PORT
--- app_module/database.js
+++ app_module/database.js
@@ -3,12 +3,53 @@ const colors = require('./colors');
 // Database for price
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-//var dbUrl = 'mongodb://localhost/';
-var dbUrl = 'mongodb://172.17.0.2/';
-var dbName = 'EVM';
 
-db.on('error', console.error);
-db.once('open', ()=>{
-	console.log( colors.info("Datatbase is connected") );
+// 실행 매개변수로부터 Database 주소와 Port를 가져온다.
+const command = process.argv;
+
+let database_ip;
+let database_port;
+process.argv.forEach(function (item, index){
+	switch( item ){
+		case '--database':
+		case '-d':
+			database_ip = process.argv[ index +1 ];
+			break;
+		case '--port':
+		case '-p':
+			database_port = process.argv[ index +1 ];
+			break;
+	}
+
+	if( database_ip == null ){
+		database_ip = '127.0.0.1';
+	}
+	
+	if( database_port == null ){
+		database_port = '27017';
+	}
 });
-mongoose.connect( dbUrl+dbName, {useNewUrlParser: true} );
+
+var dbUrl = 'mongodb://'+database_ip+':'+database_port;
+var dbName = '/EVM';
+
+var error_count = 0;
+db.on('error', (error)=>{
+	// If transient error, retry the whole transaction
+	// https://docs.mongodb.com/manual/core/transactions/#retry-transaction
+	//
+	// Shell script로 실행 시 TransientTransactionError가 발생한다.
+	// autoReconnect 옵션이 작동하지 않아 재 연결을 구현한다.
+	
+	if (error.errorLabels && error.errorLabels.indexOf('TransientTransactionError') >= 0) {
+		error_count++;
+		console.log('TransientTransactionError, retrying transaction '+ error_count +' ...');
+		mongoose.connect( dbUrl+dbName, {useNewUrlParser: true} );
+	} else {
+		throw error;
+	}
+});
+db.once('open', ()=>{
+	console.log( colors.info( dbUrl+" is opened") );
+});
+mongoose.connect( dbUrl+dbName, { useNewUrlParser: true } );
